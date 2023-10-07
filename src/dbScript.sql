291cbe041db49204204e6e9c697b3c86eeab5faa
Vincenttteng <144328225+Vincenttteng@users.noreply.github.com>
2023-10-07 12:02:48 +0800

    added a db connection

---
+++ src/dbScript.sql
@@ -0,0 +1,12 @@
+CREATE SCHEMA `db_sad2` ;
+
+REATE TABLE `db_sad2`.`tbl_users` (
+  `userID` INT(11) NOT NULL,
+  `username` VARCHAR(45) NULL,
+  `password` VARCHAR(45) NULL,
+  `lastname` VARCHAR(45) NULL,
+  `firstname` VARCHAR(45) NULL,
+  `age` INT(2) NULL,
+  `role` VARCHAR(45) NULL,
+  `isActive` TINYINT(1) NULL,
+  PRIMARY KEY (`userID`));
