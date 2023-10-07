594f544ceb17581ab2eba374d63f27ef365e5fe9
Jeru012 <144328060+Jeru012@users.noreply.github.com>
2023-10-07 00:01:40 -0400

    added DB connection

---
+++ src/libs/DBConnect.js
@@ -0,0 +1,19 @@
+const Sequelize = require("sequelize");
+
+async function connectToDB(){
+    const sequelize=new Sequelize("db_sad2","root","",{
+        host:"localhost",
+        dialect:"mysql"
+    })
+
+    await sequelize.authenticate().then(()=>{
+        //this is the resolve promise
+        console.log("DB connection is  successful!")
+
+    }).catch((error)=>{
+        //reject the promise
+        console.error("Encoutered error",err)
+    })
+}
+
+exports.connectToDB=connectToDB
