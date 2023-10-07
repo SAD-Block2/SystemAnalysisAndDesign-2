82a1557fe85f53dcfd05d603a27cd7640c43b051
g4rp-mrcl <68528199+g4rp-mrcl@users.noreply.github.com>
2023-10-07 12:02:28 +0800

    added DB connection

---
+++ src/libs/DBConnect.js
@@ -0,0 +1,19 @@
+const Sequelize = require("sequelize")
+
+async function connectToDB(){
+    const sequelize = new Sequelize("db_sad2","root "," ",{
+        host:"localhost",
+        dialect:"mysql"
+    });
+
+    await sequelize.authenticate().then(()=>{
+        //this is the resolve promise
+
+    }).catch((err)=>{
+        // reject the promise
+        console.error("Encountered error",err)
+    })
+
+}
+
+exports.connectToDB=connectToDB
