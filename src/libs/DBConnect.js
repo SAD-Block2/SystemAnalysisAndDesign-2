4018821a4c614c8148a655396bdefff706e87388
Elijah Samuel Justo <elijahsamueljusto@gmail.com>
2023-10-07 12:01:53 +0800

    Added DB Connection

---
+++ src/libs/DBConnect.js
@@ -0,0 +1,18 @@
+const Sequelize = require("sequelize")
+async function connectToDB(){
+    const sequelize = new  Sequelize("db_sad2", "root", "", {
+        host: "localhost",
+        dialect: "mysql2"
+    })
+
+    await sequelize.authenticate().then(()=>{
+        //this is the resolve promise
+        console.log("DB connection is successful!")
+    }).catch((err)=>{
+       //reject the promise
+        console.error("Encountered error", err)
+    })
+    
+}
+
+exports.connectToDB=connectToDB
