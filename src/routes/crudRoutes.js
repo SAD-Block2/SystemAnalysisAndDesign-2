961a97cf41569d555f75a768e0a73b69130aad9a
Admin <144328345+daveem15@users.noreply.github.com>
2023-10-21 11:40:42 +0800

    Revert "modified DBmodel"
    This reverts commit f3a608ccad8d6802550b539292975de607ff0a6d.

--- src/routes/crudRoutes.js
+++ src/routes/crudRoutes.js
@@ -1,5 +1,3 @@
-const DBModel=require("../lib/DBModel")
-
 const arr = [
   {
     id: 1,
@@ -18,7 +16,6 @@ const arr = [
   },
 ];
 
-
 function check(req, res) {
   var { params } = req.params;
   console.log(params);
@@ -35,32 +32,9 @@ function postMessage(req, res) {
   var body = req.body;
 
   return res.json({
-    message: body.message,
+    mesage: body.message,
   });
-}
-
-function insertUser(req, res){
-  let body=req.body
-  const{username,password,email}=body
-  try {
-    const user=DBModel.createUsers({
-      username:username,
-      password:password,
-      email:email
-    });
-    res.json({
-      user:user,
-      result:body,
-      message:"Succesfull"
-    })
-  } catch (error) {
-    res.json({
-      errorMessage:error
-    })
-    
-  }
 }
 
 exports.check = check;
 exports.postMessage = postMessage;
-exports.insertUser=insertUser;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,8 @@
-const { check, postMessage, insertUser } = require("./crudRoutes");
+const { check, postMessage } = require("./crudRoutes");
 
 function getRoutes(app) {
   app.get("/check/:params", check);
   app.post("/post", postMessage);
-  app.post("/users/create", insertUser)
 }
 
 exports.getRoutes = getRoutes;
